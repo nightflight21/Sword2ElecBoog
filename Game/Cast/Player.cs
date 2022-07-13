683d6bf492d1ed3c9e1a970aa16680952be9df8f
nightflight21 <104380966+nightflight21@users.noreply.github.com>
2022-07-13 09:55:16 -0700

    changed actor and errors it caused

--- Game/Cast/Player.cs
+++ Game/Cast/Player.cs
@@ -9,17 +9,11 @@ namespace Sword.Casting
     public class Player : Actor
     {
         private Body body;
-        private Vector2 _position = Vector2.Zero;
-        private float _rotation = 0f;
-        private float _scale = 1f;
-        private Vector2 _size = Vector2.Zero;
-        //private Color _tint = Color.White();
-        private Vector2 _velocity = Vector2.Zero;
         
         /// <summary>
         /// Constructs a new instance of Actor.
         /// </summary>
-        public Player(Body body, bool debug) : base(debug)
+        public Player(Body body)
         {
             this.body = body;
             //this.animation = animation;
@@ -47,7 +41,7 @@ namespace Sword.Casting
 
         public virtual void Steer(Vector2 vector)
         {
-            _velocity = vector;
+            //_velocity = vector;
         }
         
     }
