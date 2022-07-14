1d248241ea18efd3dd6157e823256797f2719c5d
nightflight21 <104380966+nightflight21@users.noreply.github.com>
2022-07-14 12:41:03 -0700

    idk

--- Program.cs
+++ Program.cs
@@ -54,7 +54,7 @@ namespace Sword
             scene.AddAction(Phase.Update, updateStatusAction);
             scene.AddAction(Phase.Output, drawActorsAction);
             
-            Director director = new Director(SceneManager.VideoService);
+            Director director = new Director(SceneManager.serviceFactory.GetVideoService());
             director.StartGame();
         }
     }
